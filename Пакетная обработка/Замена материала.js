71f7db4a8946ac115f4bfa3cbc8ff4971d6d9321
Let0s <Le_t0s@mail.ru>
2017-04-18 18:30:15 +0400

    update script

--- Пакетная обработка/Замена материала.js
+++ Пакетная обработка/Замена материала.js
@@ -9,8 +9,10 @@ let newMat = NewMaterialInput('Новый материал');
 let ApplyBtn = NewButtonInput('Заменить материал');
 ApplyBtn.Visible = false;
 ApplyBtn.OnChange = function () {
-    ChangeMaterial(oldMat.Name, newMat.Name, newMat.Thickness, './');
-    Action.Finish();
+    let newFolder = system.askFolder();
+    if (newFolder) {
+        ChangeMaterial(oldMat.Name, newMat.Name, newMat.Thickness, newFolder);
+    }
 }
 
 function ShowFinishBtn() {
@@ -19,31 +21,37 @@ function ShowFinishBtn() {
 let oldMatChanged = false;
 let newMatChanged = false;
 
+function ConfirmDifferentThickness() {
+    return confirm(`Материалы имеют разную толщину. Вы уверены, что хотите заменить ${FormatMatName(oldMat.Name)} 
+        с толщиной ${oldMat.Thickness} на ${FormatMatName(newMat.Name)} с толщиной ${newMat.Thickness}?`);
+}
+
 //обработчики событий выбора материала для замены 
 oldMat.OnChange = function () {
     oldMatChanged = true;
     if (newMatChanged && oldMat.Thickness != newMat.Thickness) {
-        oldMatChanged = confirm(`Материалы имеют разную толщину. Вы уверены, что хотите заменить ` +
-            `${oldMat.Name} с толщиной ${oldMat.Thickness} на ${newMat.Name} с толщиной ${newMat.Thickness}?`)
+        oldMatChanged = ConfirmDifferentThickness();
     }
     ShowFinishBtn();
 }
 newMat.OnChange = function () {
     newMatChanged = true;
     if (oldMatChanged && oldMat.Thickness != newMat.Thickness) {
-        newMatChanged = confirm(`Материалы имеют разную толщину. Вы уверены, что хотите заменить ` +
-            `${oldMat.Name} с толщиной ${oldMat.Thickness} на ${newMat.Name} с толщиной ${newMat.Thickness}?`)
+        newMatChanged = ConfirmDifferentThickness();
     }
     ShowFinishBtn();
 }
 
 function ChangeMaterial(oldName, newName, newThickness, folder) {
-    // список имен всех фалов в папке, где находится скрипт
+    // список имен всех фалов в указанной папке
     let names = fs.readdirSync(folder);
     let files = [];
     let objCount = 0;
-    // перебор всех имен файлов в папке
-    for (let fileName of names) {
+    let count = 0;
+
+    function ProcessNextFile() {
+        let fileName = names[count];
+        Action.Hint = `обработка файла ${count + 1} из ${names.length} - ${fileName}`;
         // ext - расширение файла
         let ext = path.extname(fileName);
         // проверка расширения файла
@@ -63,15 +71,24 @@ function ChangeMaterial(oldName, newName, newThickness, folder) {
                         obj.Thickness = newThickness;
                 }
             })
-            if (modelChanged){
+            if (modelChanged) {
                 Action.Commit();
                 Model.Build();
                 // Action.SaveModel(fileName);
                 files.push(folder + fileName);
             }
         }
-    }
-    alert(`Замена материалов с "${FormatMatName(oldMat.Name)}" на "${FormatMatName(newMat.Name)}" 
-     произведена на ${objCount} объектах в ${files.length} файлах:\n${files.join('\n')}`);
+        count++;
+        if (count < names.length) {
+            Action.AsyncExec(ProcessNextFile);
+        }
+        else {
+            alert(`Замена материалов с "${FormatMatName(oldMat.Name)}" на "${FormatMatName(newMat.Name)}" 
+                произведена на ${objCount} объектах в ${files.length} файлах:\n${files.join('\n')}`);
+            Action.Finish();
+        }
+    };
+    ProcessNextFile();
 }
+
 Action.Continue();

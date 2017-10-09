7ff19803bb7ef93fce524e259e3d1de2021eba83
Sergey Meshchaninov <Le_t0s@mail.ru>
2017-10-09 16:28:33 +0400

    fix module files, add sample script

---
+++ Примеры работы с модулями/Сохранение выделенного объекта в отдельный файл.ts
@@ -0,0 +1,11 @@
+﻿// модуль bazis_module должен находиться в папке node_modules
+import * as baz from 'bazis_module';
+let obj = Model.Selected;
+if (obj){
+    let openedFile = Action.ModelFilename;
+    let newFileName = system.askFileNameSave('b3d');
+    if (newFileName){
+        baz.saving.SaveObject(obj, newFileName);
+        Action.LoadModel(openedFile);
+    }
+}
